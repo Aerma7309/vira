001ae554fb181278be41268b5cf669386d2e09e3
Abhimanyu Kumar Maurya <aerma7309@gmail.com>
2025-10-20 09:11:21 +0530

    fix

--- nix/modules/home-manager/vira.nix
+++ nix/modules/home-manager/vira.nix
@@ -42,6 +42,7 @@ in
         # Environment
         Environment = [
           "PATH=${makeBinPath cfg.extraPackages}:$PATH"
+          "GIT_SSH_COMMAND=ssh -F %h/.ssh/config -o Include=none"
         ];
       };
 
